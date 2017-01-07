b45d895844ea315b1fd4e990a47d3c569bcbee89
DangMinh24 <dangtm24@gmail.com>
2017-01-06 21:45:13 -0600

    Update stanford_parser.py

--- stanford_parser.py
+++ stanford_parser.py
@@ -1,5 +1,5 @@
 from nltk.parse.stanford import StanfordParser
-STANFORD_JAR="/home/dang/Desktop/stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0.jar"
-STANFORD_MODEL="/home/dang/Desktop/stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0-models.jar"
+STANFORD_JAR="[userlocation]/stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0.jar"
+STANFORD_MODEL="[userlocation]/stanford-corenlp-full-2016-10-31/stanford-corenlp-3.7.0-models.jar"
 def SFParser(jar_path,model_path):
     return StanfordParser(jar_path,model_path)
