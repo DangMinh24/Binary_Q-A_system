6626e2a345008dc4e500b321ffffa25f3694e86d
DangMinh24 <dangtm24@gmail.com>
2017-01-06 21:44:41 -0600

    Update stanford_ner.py

--- stanford_ner.py
+++ stanford_ner.py
@@ -1,7 +1,7 @@
 from nltk.tag.stanford import StanfordNERTagger
 
-NER_CF="/home/dang/Desktop/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz"
-NER_JAR="/home/dang/Desktop/stanford-ner-2015-12-09/stanford-ner.jar"
+NER_CF="[userlocation]/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz"
+NER_JAR="[userlocation]/stanford-ner-2015-12-09/stanford-ner.jar"
 def SFNer(classifier_path,jar_path):
     return StanfordNERTagger(classifier_path,jar_path)
 from nltk.tokenize import word_tokenize
